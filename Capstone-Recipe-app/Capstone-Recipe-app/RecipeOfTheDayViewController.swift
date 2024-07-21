8c1c33e786a1e6db360e4adfd75be1be541a7dc1
Tir7hshah <152945657+Tir7hshah@users.noreply.github.com>
2024-07-21 18:06:27 -0400

    #8 impliments recipe of the day
    closes #8

---
+++ Capstone-Recipe-app/Capstone-Recipe-app/RecipeOfTheDayViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RecipeOfTheDayViewController.swift
+//  Capstone-Recipe-app
+//
+//  Created by Tirth Shah on 2024-07-21.
+//
+
+import UIKit
+
+class RecipeOfTheDayViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var recipes = [Recipe]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadRecipes()
+    }
+    
+    private func loadRecipes() {
+        RecipeService.shared.loadRecipes { [weak self] loadedRecipes in
+            self?.recipes = loadedRecipes
+            self?.setRecipeOfTheDay()
+        }
+    }
+    
+    private func setRecipeOfTheDay() {
+        guard !recipes.isEmpty else { return }
+        if let randomRecipe = recipes.randomElement() {
+            updateUI(with: randomRecipe)
+        }
+    }
+    
+    private func updateUI(with recipe: Recipe) {
+        titleLabel.text = recipe.title
+        descriptionLabel.text = recipe.description
+        imageView.image = UIImage(named: recipe.imageName)
+    }
+}
--- Capstone-Recipe-app/RecipeService.swift
+++ Capstone-Recipe-app/RecipeService.swift
@@ -1,3 +1,4 @@
+
 import Foundation
 
 class RecipeService {
@@ -36,3 +37,4 @@ class RecipeService {
         }
     }
 }
+
